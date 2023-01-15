d94357d2005ba8cfaabd216679b6f51b46111bb0
Gabor Szita <gaborszita88@gmail.com>
2023-01-15 15:50:35 -0800

    add javadocs to methods

--- src/main/include/AprilTagPoseEstimator.h
+++ src/main/include/AprilTagPoseEstimator.h
@@ -6,7 +6,13 @@
 
 class AprilTagPoseEstimator {
 public:
-    frc::Pose2d getPose(frc::Pose3d aprilTagPos, int aprilTagNum);
+    /**
+     * Returns the robot position based on an april tag position reading.
+     * @param aprilTagPosReading april tag position reading @see AprilTagPoseEstimator.cpp for more details about this argument
+     * @param aprilTagNum april tag number
+     * @return robot position
+    */
+    frc::Pose2d getPose(frc::Pose3d aprilTagPosReading, int aprilTagNum);
 
 private:
     frc::AprilTagFieldLayout aprilTagFieldLayout = AprilTags::getAprilTagFieldLayout();
--- src/main/include/AprilTags.h
+++ src/main/include/AprilTags.h
@@ -2,5 +2,9 @@
 
 class AprilTags {
 public:
+    /**
+     * Returns the april tag field layout of the 2023 Charged Up game.
+     * @return april tag field layout
+    */
     static frc::AprilTagFieldLayout getAprilTagFieldLayout();
 };
