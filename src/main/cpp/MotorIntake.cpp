668156e7b0eccd04b9cd99ce53caf3d334d7f173
Jonathan Liu <jonathanhliu21@gmail.com>
2023-03-02 17:57:04 -0800

    reorder implementations

--- src/main/cpp/MotorIntake.cpp
+++ src/main/cpp/MotorIntake.cpp
@@ -1,85 +1,14 @@
 #include "MotorIntake.h"
 
+/**
+ * Constructor
+ *
+ * @param showSmartDashboard Set true if state machines should be shown to SmartDashboard
+ */
 MotorIntake::MotorIntake(bool showSmartDashboard) : m_showSmartDashboard{showSmartDashboard}
 {
   m_pid.SetTolerance(units::radian_t{MotorIntakeConstants::POS_ERR_TOLERANCE},
                      units::radians_per_second_t{MotorIntakeConstants::VEL_ERR_TOLERANCE});
-}
-
-/**
- * Causes the intake to go down
- */
-void MotorIntake::Deploy()
-{
-  m_deployerState = DEPLOYING;
-  ResetPID();
-  ResetAcceleration();
-}
-
-/**
- * Causes the intake to come up
- */
-void MotorIntake::Stow()
-{
-  m_deployerState = STOWING;
-  ResetPID();
-  ResetAcceleration();
-}
-
-MotorIntake::DeployerState MotorIntake::getDeployerState()
-{
-  return m_deployerState;
-}
-
-MotorIntake::RollerState MotorIntake::getRollerState()
-{
-  return m_rollerState;
-}
-
-void MotorIntake::ResetEncoderPosition()
-{
-  m_deployerMotor.GetEncoder().SetPosition(0);
-}
-
-/**
- * Resets intake. This includes:
- *
- * - resetting the encoder position
- * - resetting the state so that the robot thinks the arm is stowed
- *
- * Note that this does not move any motor positions.
- *
- * This should be called on init (the RobotInit() method for this class does that for you).
- */
-void MotorIntake::Reset()
-{
-  ResetEncoderPosition();
-  ResetPID();
-  ResetAcceleration();
-  m_deployerState = STOWED;
-  m_rollerState = STOP;
-  m_intakeState = IDLE;
-}
-
-/**
- * Resets the PID
- *
- * If using this along with ResetEncoderPosition(), call
- * ResetPID() AFTER ResetEncoderPosition()
- */
-void MotorIntake::ResetPID()
-{
-  m_pid.Reset(units::radian_t(m_deployerMotor.GetEncoder().GetPosition()));
-}
-
-/**
- * Resets acceleration calculations (currently unused; may use in the future
- * in case we need feedforward calculations)
- */
-void MotorIntake::ResetAcceleration()
-{
-  m_lastTime = frc::Timer::GetFPGATimestamp();
-  m_lastSpeed = units::radians_per_second_t{0};
 }
 
 /**
@@ -88,85 +17,6 @@ void MotorIntake::ResetAcceleration()
 void MotorIntake::RobotInit()
 {
   Reset();
-}
-
-bool MotorIntake::m_MotorsNotNeeded()
-{
-  return m_intakeState == IDLE || m_intakeState == CONSUMED;
-}
-
-void MotorIntake::m_DeployerStateMachine()
-{
-  if (m_MotorsNotNeeded())
-  {
-    m_deployerMotor.SetVoltage(units::volt_t{0});
-    return;
-  }
-
-  switch (m_deployerState)
-  {
-  case STOWED:
-    m_deployerMotor.SetVoltage(units::volt_t{0});
-
-    break;
-  case DEPLOYED:
-    m_deployerMotor.SetVoltage(units::volt_t{0});
-
-    break;
-  case DEPLOYING:
-  {
-    // calculate motion-profiled PID for deploying
-    double pidVal = m_pid.Calculate(m_getEncoderRadians(),
-                                    Helpers::convertStepsToRadians(MotorIntakeConstants::ENCODER_DEPLOYED_TARGET, 2048));
-    double voltage = std::clamp(pidVal, -MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE, MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE);
-
-    m_deployerMotor.SetVoltage(units::volt_t{voltage});
-
-    if (m_pid.AtGoal())
-    {
-      m_deployerState = DEPLOYED;
-    }
-
-    break;
-  }
-  case STOWING:
-  {
-    // calculate motion-profiled PID for stowing
-    double pidVal = m_pid.Calculate(m_getEncoderRadians(), units::radian_t{0});
-    double voltage = std::clamp(pidVal, -MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE, MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE);
-
-    m_deployerMotor.SetVoltage(units::volt_t{voltage});
-
-    if (m_pid.AtGoal())
-    {
-      m_deployerState = STOWED;
-    }
-
-    break;
-  }
-  }
-}
-
-void MotorIntake::m_RollerStateMachine()
-{
-  if (m_deployerState != DEPLOYED || m_MotorsNotNeeded() ||
-      m_rollerMotor.GetSupplyCurrent() >= MotorIntakeConstants::ROLLER_STALL_CURRENT)
-  {
-    m_rollerMotor.SetVoltage(units::volt_t(0));
-    return;
-  }
-
-  switch (m_rollerState)
-  {
-  case INTAKE:
-    m_rollerMotor.SetVoltage(units::volt_t(MotorIntakeConstants::ROLLER_INTAKE_VOLTAGE));
-  case OUTTAKE:
-    m_rollerMotor.SetVoltage(units::volt_t(MotorIntakeConstants::ROLLER_OUTTAKE_VOLTAGE));
-  case STOP:
-    m_rollerMotor.SetVoltage(units::volt_t(0));
-  default:
-    m_rollerMotor.SetVoltage(units::volt_t(0));
-  }
 }
 
 /**
@@ -263,14 +113,6 @@ bool MotorIntake::IsStored()
 }
 
 /**
- * If the cone intake is stowed without a cone inside.
- */
-bool MotorIntake::IsIdle()
-{
-  return m_intakeState == IDLE;
-}
-
-/**
  * If the intake is down and currently spitting out the cone.
  *
  * This method can be used to detect if the cone is ready to intake;
@@ -281,6 +123,169 @@ bool MotorIntake::IsIdle()
 bool MotorIntake::IntakeDown()
 {
   return m_intakeState == INTAKE_DOWN;
+}
+
+/**
+ * If the cone intake is stowed without a cone inside.
+ */
+bool MotorIntake::IsIdle()
+{
+  return m_intakeState == IDLE;
+}
+
+MotorIntake::DeployerState MotorIntake::getDeployerState()
+{
+  return m_deployerState;
+}
+
+MotorIntake::RollerState MotorIntake::getRollerState()
+{
+  return m_rollerState;
+}
+
+/**
+ * Causes the intake to go down
+ */
+void MotorIntake::Deploy()
+{
+  m_deployerState = DEPLOYING;
+  ResetPID();
+  ResetAcceleration();
+}
+
+/**
+ * Causes the intake to come up
+ */
+void MotorIntake::Stow()
+{
+  m_deployerState = STOWING;
+  ResetPID();
+  ResetAcceleration();
+}
+
+void MotorIntake::ResetEncoderPosition()
+{
+  m_deployerMotor.GetEncoder().SetPosition(0);
+}
+
+/**
+ * Resets intake. This includes:
+ *
+ * - resetting the encoder position
+ * - resetting the state so that the robot thinks the arm is stowed
+ *
+ * Note that this does not move any motor positions.
+ *
+ * This should be called on init (the RobotInit() method for this class does that for you).
+ */
+void MotorIntake::Reset()
+{
+  ResetEncoderPosition();
+  ResetPID();
+  ResetAcceleration();
+  m_deployerState = STOWED;
+  m_rollerState = STOP;
+  m_intakeState = IDLE;
+}
+
+/**
+ * Resets the PID
+ *
+ * If using this along with ResetEncoderPosition(), call
+ * ResetPID() AFTER ResetEncoderPosition()
+ */
+void MotorIntake::ResetPID()
+{
+  m_pid.Reset(units::radian_t(m_deployerMotor.GetEncoder().GetPosition()));
+}
+
+/**
+ * Resets acceleration calculations (currently unused; may use in the future
+ * in case we need feedforward calculations)
+ */
+void MotorIntake::ResetAcceleration()
+{
+  m_lastTime = frc::Timer::GetFPGATimestamp();
+  m_lastSpeed = units::radians_per_second_t{0};
+}
+
+bool MotorIntake::m_MotorsNotNeeded()
+{
+  return m_intakeState == IDLE || m_intakeState == CONSUMED;
+}
+
+void MotorIntake::m_DeployerStateMachine()
+{
+  if (m_MotorsNotNeeded())
+  {
+    m_deployerMotor.SetVoltage(units::volt_t{0});
+    return;
+  }
+
+  switch (m_deployerState)
+  {
+  case STOWED:
+    m_deployerMotor.SetVoltage(units::volt_t{0});
+
+    break;
+  case DEPLOYED:
+    m_deployerMotor.SetVoltage(units::volt_t{0});
+
+    break;
+  case DEPLOYING:
+  {
+    // calculate motion-profiled PID for deploying
+    double pidVal = m_pid.Calculate(m_getEncoderRadians(),
+                                    Helpers::convertStepsToRadians(MotorIntakeConstants::ENCODER_DEPLOYED_TARGET, 2048));
+    double voltage = std::clamp(pidVal, -MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE, MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE);
+
+    m_deployerMotor.SetVoltage(units::volt_t{voltage});
+
+    if (m_pid.AtGoal())
+    {
+      m_deployerState = DEPLOYED;
+    }
+
+    break;
+  }
+  case STOWING:
+  {
+    // calculate motion-profiled PID for stowing
+    double pidVal = m_pid.Calculate(m_getEncoderRadians(), units::radian_t{0});
+    double voltage = std::clamp(pidVal, -MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE, MotorIntakeConstants::DEPLOYER_MAX_VOLTAGE);
+
+    m_deployerMotor.SetVoltage(units::volt_t{voltage});
+
+    if (m_pid.AtGoal())
+    {
+      m_deployerState = STOWED;
+    }
+
+    break;
+  }
+  }
+}
+
+void MotorIntake::m_RollerStateMachine()
+{
+  if (m_deployerState != DEPLOYED || m_MotorsNotNeeded() ||
+      m_rollerMotor.GetSupplyCurrent() >= MotorIntakeConstants::ROLLER_STALL_CURRENT)
+  {
+    m_rollerMotor.SetVoltage(units::volt_t(0));
+    return;
+  }
+
+  switch (m_rollerState)
+  {
+  case INTAKE:
+    m_rollerMotor.SetVoltage(units::volt_t(MotorIntakeConstants::ROLLER_INTAKE_VOLTAGE));
+  case OUTTAKE:
+    m_rollerMotor.SetVoltage(units::volt_t(MotorIntakeConstants::ROLLER_OUTTAKE_VOLTAGE));
+  case STOP:
+    m_rollerMotor.SetVoltage(units::volt_t(0));
+  default:
+    m_rollerMotor.SetVoltage(units::volt_t(0));
+  }
 }
 
 units::radian_t MotorIntake::m_getEncoderRadians()
